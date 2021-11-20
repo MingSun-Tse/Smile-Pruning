3f2c2a16b42d46b2b75f8ffe6bd98a057a50d24b
Xingchen Zhao <xiz168@pitt.edu>
2021-11-20 17:04:10 -0500

    added epoch print when printing jsv

--- main.py
+++ main.py
@@ -478,8 +478,8 @@ def finetune(model, train_loader, val_loader, train_sampler, criterion, pruner, 
         # @mst: check Jacobian singular value (JSV)
         if args.jsv_loop:
             jsv, cn = get_jacobian_singular_values(model, train_loader, num_classes=num_classes, n_loop=args.jsv_loop, print_func=print, rand_data=args.jsv_rand_data)
-            print('Epoch %d JSV_mean %.4f JSV_std %.4f JSV_max %.4f JSV_min %.4f Condition_Number_mean %.4f' % 
-                (epoch, np.mean(jsv), np.std(jsv), np.max(jsv), np.min(jsv), np.mean(cn)))
+            print('JSV_mean %.4f JSV_std %.4f JSV_max %.4f JSV_min %.4f Condition_Number_mean %.4f -- Epoch %d' % 
+                (np.mean(jsv), np.std(jsv), np.max(jsv), np.min(jsv), np.mean(cn), epoch))
 
         # @mst: check weights magnitude during finetune
         if args.method in ['GReg-1', 'GReg-2'] and not isinstance(pruner, type(None)):
